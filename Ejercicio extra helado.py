b708efb4eb15d4fc4944bb0bd691ae315c4ad779
A01376572 <61155013+A01376572@users.noreply.github.com>
2020-02-20 09:48:12 -0600

    Ejercicio extra helado, Daniel Rojas, A01376572
    Ejercicio extra

---
+++ Ejercicio extra helado.py
@@ -0,0 +1,8 @@
+# Autor: Daniel Rojas
+# Calcular el área de un helado
+R = int( input("Teclea el radio: "))
+h = int( input("Teclea la altura: "))
+
+area=(3.1416*R**2)/2+(R*2*h)/2
+
+print ("El área es: ", area)
