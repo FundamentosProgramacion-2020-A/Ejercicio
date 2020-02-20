6e7eeef991c60dbdd1965e08e4be6b0292e9ed37
a01748774 <61155048+a01748774@users.noreply.github.com>
2020-02-20 09:49:19 -0600

    Add files via upload

---
+++ Calculo de área.py
@@ -0,0 +1,7 @@
+#Autor: Valeria Huerta
+#Calcular área de superficie
+
+r=int(input("Teclea el valor del radio "))
+h=int(input("Teclea el valor de la altura "))
+area= ((3.1416*r**2)/2) + ((2*r*h)/2)
+print("El área de la superficie es: ",area)
