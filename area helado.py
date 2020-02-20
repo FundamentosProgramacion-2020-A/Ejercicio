1b6a594bf321cafdbbcd81184c5091138cc0a13f
A01333976 <61155066+A01333976@users.noreply.github.com>
2020-02-20 09:50:21 -0600

    Ejerciciohelado,A01333976

---
+++ area helado.py
@@ -0,0 +1,16 @@
+#Autor:Maximiliano Garcia
+#Ejercicio helado
+
+r1=int(input("leer r:"))
+h=int(input("leer h:"))
+
+ac=(1/2)*3.14*r1
+at=2*r1*h/2
+
+ah=ac+at
+
+print("Area circulo",ac)
+print("Area triangulo",at)
+print("Area helado",ah)
+
+
