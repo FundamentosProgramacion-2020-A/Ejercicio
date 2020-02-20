1d4269c8fde67789f1831de56a169e839194ba70
A01376720 <61218550+A01376720@users.noreply.github.com>
2020-02-20 09:45:46 -0600

    Ejercicio, Andy Martinez Hernández, A01376720
    ejercicio extra de la clase

---
+++ 5.- extra.py
@@ -0,0 +1,16 @@
+#Autor: Andy P. Martínez Hernández
+#Ejercicio extra
+
+#variables
+r= int (input ("Inserta radio de círculo"))
+h= int (input ("Inserta altura"))
+pi= 3.14
+
+#Areas
+Acirculo= (pi*r**2)/2
+Atrian= ((r*2)*h)/2
+
+#Suma areas
+Atotal= Acirculo + Atrian
+
+print (Atotal)
