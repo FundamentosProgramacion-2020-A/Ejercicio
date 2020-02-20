b8bd73d55bd802977dc3c294eae6d25633342c24
A0146107 <61154928+Mariana697@users.noreply.github.com>
2020-02-20 09:53:03 -0600

    Mariana Ponce González A01746107

---
+++ helado.py
@@ -0,0 +1,9 @@
+#Autor: Mariana Ponce González
+# Calcula datos del helado
+
+r = int(input("Dame el radio "))
+h = int(input("Dame la altura "))
+
+a = (3.1416*r**2)/2+(3**0.5*(r*2)**2/4)
+
+print ("Area del elado es ", a)
