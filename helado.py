35b294579ec542a397f533493be820b4302b6e6e
A01371954 <61154949+A01371954@users.noreply.github.com>
2020-02-20 09:45:24 -0600

    helado, Roberto Sobrado, A01371954

---
+++ helado.py
@@ -0,0 +1,8 @@
+#Roberto SObrado
+#area helado
+r= int( input ("¿Cuál es el radio de la bola de helado?"))
+h= int( input ("Cuál es la altura del cono?"))
+
+a=((3.1416*r**2)/2)+(((r*2)*h))/2
+
+print ("el area del helado es: ", a)
