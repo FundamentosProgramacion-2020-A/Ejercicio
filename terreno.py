cb8864abeea519ee9d23b8f5149fa9e9744f6ae7
A01371954 <61154949+A01371954@users.noreply.github.com>
2020-02-20 09:03:51 -0600

    Add files via upload

---
+++ terreno.py
@@ -0,0 +1,11 @@
+#Roberto Sobrado
+#area del terreno
+a= int(input ("¿Cuál es la distancia de la base larga?"))
+b= int(input ("¿Cuál es la distancia de la base corta?"))
+h= int(input ("¿Cuánto mide la altura del terreno?"))
+area= h*((a+b)/2)
+precio= area*3450
+print ("el area es:", area, "m2")
+print ("el precio total del terren es: $", precio)
+
+       
