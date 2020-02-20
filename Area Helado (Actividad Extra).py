1bd43e9cc0670200adc843177560aeeed8e27f1c
A01376726 <61155105+A01376726@users.noreply.github.com>
2020-02-20 09:48:27 -0600

    Add files via upload

---
+++ Area Helado (Actividad Extra).py
@@ -0,0 +1,14 @@
+# Autor José Manuel Rivera Ssosapavón
+# Ejercicio Extra
+
+r = int(input("Radio ="))
+h = int(input("Altura ="))
+b = r*2
+     
+a1 = (3.1416*r**2)/2
+a2 = (b*h)/2
+     
+at = a1 + a2
+     
+print ("area del helado =", at)
+     
