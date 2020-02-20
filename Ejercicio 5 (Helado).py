82b280c8f8bed9554508df9c5bff74c497886dbd
A01376318 <61154988+A01376318@users.noreply.github.com>
2020-02-20 09:45:23 -0600

    Ejercicio EXTRA
    Ejercicio del helado

---
+++ Ejercicio 5 (Helado).py
@@ -0,0 +1,11 @@
+#Autor: Elena R.Tovar
+#Calcular el área del helado
+
+pi = 3.1416
+r= int(input("Ingrese radio: "))
+h= int(input("Ingrese altura "))
+d=r*2
+aC= float (pi*r**2)
+aT= float ((d*h)/2)
+A= float (at+(aC/2))
+print ("El área de la figura es: ", A)
